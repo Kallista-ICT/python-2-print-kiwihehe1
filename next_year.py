700c0264639c30eb88523c4150b3c705b6188d92
kiwihehe1 <kstephanie0602@gmail.com>
2025-02-11 07:20:28 +0000

    Submit

--- next_year.py
+++ next_year.py
@@ -1,6 +1,9 @@
 print("Hello, World!")
-name = input("Kelly")
-print(f"Hello{name}")
+name = input("Enter your name: ")
+print(f"Hello {name}")
+age = int(input("Enter your age: "))
+print(f"Hello, {name}! You are {age} years old.")
+print(f"Next year you will be {age + 1} years old.")
 
 
 
